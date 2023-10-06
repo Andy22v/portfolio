014f7073d08a4bd09619b593e1225a7f237cb186
andy <andy44v@gmail.com>
2023-10-06 16:59:42 +0200

    feat hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,10 +9,9 @@ const Hero = () => {
           <div className={styles.Hero__info}>
             <h3>👋 Hi, i'm Andy</h3>
             <p>
-              Hello, I'm a Front-End developer passionate about crafting
-              engaging and user-friendly web experiences. I'm dedicated to
-              continually improving my skills and knowledge in the world of web
-              development.
+              I'm a Front-End developer passionate about crafting engaging and
+              user-friendly web experiences. I'm dedicated to continually
+              improving my skills and knowledge in the world of web development.
             </p>
           </div>
           <a href="https://drive.google.com/file/d/1dEe1F_y4iK2PTQ_hSo3QCUm-5NrILl0Q/view?usp=drive_link">
